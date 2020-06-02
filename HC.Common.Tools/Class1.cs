a45859e9a98e52211465adf0261a0361c96af97d
xlg <1404049694@qq.com>
2020-06-02 17:47:03 +0800

    添加项目文件。

---
+++ HC.Common.Tools/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HC.Common.Tools
+{
+    public class Class1
+    {
+    }
+}
---
+++ HC.Core.Centre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HC.Core.Centre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
